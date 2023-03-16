cc912737f2c1f034da6c92e516bdd13fe47e93d1
xiangcitoZ <pzxiang1@gmail.com>
2023-03-16 14:24:57 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapatillaController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers
+{
+    public class ZapatillaController : Controller
+    {
+        private RepositoryZapatillas repo;
+
+        public ZapatillaController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetZapatillas()
+        {
+            List<Zapatilla> zapatillas =  await this.repo.GetZapatilla();
+            return View(zapatillas);
+        }
+
+
+
+        public async Task<IActionResult> DetailsZapatilla(int idproducto)
+        {
+            Zapatilla zapatilla = await this.repo.GetZapatilla(idproducto);
+            return View(zapatilla);
+        }
+
+        public async Task<IActionResult> ImagenZapatilla(int? posicion, int idproducto)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+
+            List<ImagenZapatilla> imagenzapatilla =
+                    await this.repo.GetImagenZapatillaAsync(posicion.Value, idproducto);
+            ViewData["REGISTROS"] = this.repo.GetNumeroImagenZapatilla(idproducto);
+            ViewData["IDPRODUCTO"] = idproducto;
+            return View(imagenzapatilla);
+        }
+
+        public async Task<IActionResult> _DetailsS(int idproducto)
+        {
+
+            Zapatilla zapatilla = await this.repo.GetZapatilla(idproducto);
+            return PartialView("_DetailsS", zapatilla);
+        }
+
+
+        
+      
+
+
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region SQL SERVER
+//CREATE PROCEDURE SP_IMAGEN_ZAPATILLA_PAGINAR
+//(@POSICION INT, @IDPRODUCTO INT)
+//AS
+//    SELECT idImagen, idProducto, imagen FROM
+//        (SELECT CAST(
+//            ROW_NUMBER() OVER(ORDER BY imagen) AS INT) AS POSICION,
+//            idImagen, idProducto, imagen
+//        FROM IMAGENESZAPASPRACTICA
+//        WHERE idProducto = @IDPRODUCTO) AS QUERY
+//    WHERE QUERY.POSICION >= @POSICION AND QUERY.POSICION < (@POSICION + 1)
+//GO
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Zapatilla>> GetZapatilla()
+        {
+            return await this.context.Zapatillas.ToListAsync();
+        }
+
+        public async Task<Zapatilla> GetZapatilla(int idproducto)
+        {
+            Zapatilla zapatilla = await this.context.Zapatillas.
+                FirstOrDefaultAsync(z => z.IdProducto == idproducto);
+            return zapatilla;
+        }
+
+        public int GetNumeroImagenZapatilla(int idproducto)
+        {
+            return this.context.ImagenesZapatillas.
+                Where(z => z.IdProducto == idproducto).Count();
+        }
+
+        public async Task<List<ImagenZapatilla>>
+        GetImagenZapatillaAsync(int posicion, int idproducto)
+        {
+            string sql =
+                "SP_IMAGEN_ZAPATILLA_PAGINAR @POSICION, @IDPRODUCTO";
+            SqlParameter pamposicion =
+                new SqlParameter("@POSICION", posicion);
+            SqlParameter pamidimagen =
+                new SqlParameter("@IDPRODUCTO", idproducto);
+
+            var consulta =
+                this.context.ImagenesZapatillas.FromSqlRaw(sql, pamposicion, pamidimagen);
+            List<ImagenZapatilla> zapatillas = await consulta.ToListAsync();
+
+            return zapatillas;
+        }
+
+       
+
+
+    }
+}
